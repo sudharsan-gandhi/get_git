16ef2bf7fcfb953dbde86dd380a40dc6d69a81e4
sudharsan gandhi <sudharsan.gandhi@ogonconsulting.com>
2023-04-24 12:35:57 -0400

    get git logic with bin registeration.

---
+++ ReadMe.md
@@ -0,0 +1,33 @@
+## GET GIT
+
+This utility is for people who want to clone multiple git projects from same domain with multiple accounts. switching between accounts is proven to be harder and tough. 
+Setting up SSH is also a redundant and hard job to do.
+
+#### Prerequisities and knowledge:
+
+* create access token from github. go to developer settings -> access token (classic) -> provide necessary access. Copy the password and have it somewhere since you can view it again.
+![Generate token](doc-assets/generate_keys.png)
+* username
+* https url to repo
+* git installed in system
+
+
+#### installation
+
+```
+npm i -g get_git
+```
+
+#### running the application
+
+```
+getgit
+```
+
+or use alias
+
+```
+gg
+```
+
+it will prompt for questions and collect details to download the application
---
+++ src/index.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+import {Command} from 'commander';
+import {execSync} from 'child_process';
+import inquirer from 'inquirer';
+import {join} from 'path';
+
+interface Answers {
+  gitUrl: string;
+  username: string;
+  password: string;
+  foldername?: string;
+}
+
+const program = new Command();
+
+program
+  .name('getgit')
+  .alias('gg')
+  .description('Clone a Git repository with username and password')
+  .action(async () => {
+    const answers: Answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'gitUrl',
+        message: 'Enter Git HTTPS URL:',
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'Enter Git username:',
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Enter Git password:',
+      },
+      {
+        type: 'input',
+        name: 'foldername',
+        message: 'Enter folder name to clone into (optional):',
+      },
+    ]);
+
+    const {gitUrl, username, password, foldername} = answers;
+    const url = new URL(gitUrl);
+    url.username = username;
+    url.password = password;
+
+    const cmd = `git clone ${url.href}`;
+    const cwd = foldername ? join(process.cwd(), foldername) : process.cwd();
+
+    execSync(cmd, {stdio: 'inherit', cwd});
+  });
+
+program.parse(process.argv);
